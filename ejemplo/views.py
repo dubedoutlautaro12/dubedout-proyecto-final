35399e7114be002b1d0a325df2b248fd31bf8c38
Lautaro Dubedout <lautydube@gmail.com>
2022-12-12 00:12:03 -0300

    A VER AHORA

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from ejemplo.models import Familiar
 def index(request):
     return render(request, "ejemplo/saludar.html")
 
@@ -24,3 +24,18 @@ def sumar(request, a, b):
      'b':b,
     'resultado': a+b}
     )
+
+def buscar(request):
+    lista_de_nombre = ['German', 'Daniel', 'Romero', 'Alvaro']    
+    query = request.GET['q']
+    if query in lista_de_nombre:
+        indice_del_resultado = lista_de_nombre.index(query)
+        resultado= lista_de_nombre[indice_del_resultado]
+    else:
+        resultado = 'NO HAY MATCH'
+    return render(request, 'ejemplo/buscar.html', {'resultado': resultado})
+    
+
+def monstrar_familiares(request):
+  lista_familiares = Familiar.objects.all()
+  return render(request, "ejemplo/familiares.html", {"lista_familiares": lista_familiares})
