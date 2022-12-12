992672326b2c8bfe39b363f7487f8c5af29706f6
Lautaro Dubedout <lautydube@gmail.com>
2022-12-12 14:23:11 -0300

    ACTUALIZACION PARA REENTREGA

---
+++ seed_data.py
@@ -0,0 +1,6 @@
+from ejemplo.models import Familiar
+Familiar(nombre="Nilda", direccion="Donizetti 988", numero_pasaporte=123123).save()
+Familiar(nombre="Carlos", direccion="Ramon Falcon 467", numero_pasaporte=890890).save()
+Familiar(nombre="Alejandro", direccion="Av Peron 432", numero_pasaporte=345345).save()
+Familiar(nombre="Natalia", direccion="Manzoni 232", numero_pasaporte=567567).save()
+print("Se cargo con éxito los usuarios de pruebas")
