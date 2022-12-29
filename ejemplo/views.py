cbd2143ec43831810e2dac7d6e2b29389876899d
Lautaro Dubedout <lautydube@gmail.com>
2022-12-29 01:11:23 -0300

    HASTA Actualizar VEHICULO.

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -233,3 +233,28 @@ class AltaVehiculo(View):
         
         return render(request, self.template_name, {"form": form})
         
+
+
+class ActualizarVehiculo(View):
+  form_class = VehiculoForm
+  template_name = 'ejemplo/actualizar_vehiculo.html'
+  initial = {'dueno':'', 'tipo':'', 'patente':''}
+  
+  def get (self, request, pk): 
+      vehiculo = get_object_or_404(Vehiculo, pk=pk)
+      form = self.form_class(instance=vehiculo)
+      return render(request, self.template_name, {'form':form,'vehiculo': vehiculo})
+
+ 
+  def post(self, request, pk): 
+      vehiculo = get_object_or_404(Vehiculo, pk=pk)
+      form = self.form_class(request.POST ,instance=vehiculo)
+      if form.is_valid(): 
+          form.save()
+          msg_exito = f"se actualizó con éxito la informacion del {form.cleaned_data.get('tipo')} de {form.cleaned_data.get('dueno')}"
+          form = self.form_class(initial=self.initial)
+          return render(request, self.template_name, {'form':form, 
+                                                      'vehiculo': vehiculo,
+                                                      'msg_exito': msg_exito})
+      
+      return render(request, self.template_name, {"form": form})
--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ from ejemplo.views import (buscar,
                              monstrar_familiares, mostrar_mascotas,
                             BuscarFamiliar, AltaFamiliar, ActualizarFamiliar,
                             BorrarFamiliar, BuscarMascota, AltaMascota, ActualizarMascota,BorrarMascota, pagina_principal,
-                            mostrar_vehiculo, BuscarVehiculo, AltaVehiculo)
+                            mostrar_vehiculo, BuscarVehiculo, AltaVehiculo, ActualizarVehiculo)
 
 
 urlpatterns = [
@@ -38,7 +38,8 @@ urlpatterns = [
     path('pagina-principal/', pagina_principal),
     path('vehiculos/', mostrar_vehiculo),
     path('vehiculos/buscar', BuscarVehiculo.as_view()),
-    path('vehiculos/alta', AltaVehiculo.as_view())
+    path('vehiculos/alta', AltaVehiculo.as_view()),
+    path("vehiculos/actualizar/<int:pk>", ActualizarVehiculo.as_view())
 ]
 
 
